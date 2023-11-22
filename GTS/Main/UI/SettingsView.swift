526627e09220bd433caa4d763138e97893594862
nicoletanyt <nicole_tan_yitong@s2021.ssts.edu.sg>
2023-11-22 13:18:56 +0800

    inc camp is great cos amongus is amonging

--- GTS/Main/UI/SettingsView.swift
+++ GTS/Main/UI/SettingsView.swift
@@ -8,8 +8,48 @@
 import SwiftUI
 
 struct SettingsView: View {
+    @State var toggle_Privacy = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            VStack {
+                Image(systemName: "photo")
+                Text("Bio")
+                Text("Count: ")
+            }
+            List {
+                Button {
+                    toggle_Privacy.toggle()
+                } label: {
+                    Text("Privacy Policy")
+                }
+            }
+        }
+        .sheet(isPresented: $toggle_Privacy) {
+            ScrollView {
+                VStack {
+                    ForEach((1...4), id: \.self) {
+                        Spacer()
+                            .frame(height: 300 * CGFloat($0))
+                        Text("scroll down")
+                        Spacer()
+                            .frame(height: 300 / CGFloat($0))
+                    }
+                    HStack {
+                        Button {
+                            toggle_Privacy = false
+                        } label: {
+                            Text("i don't understand")
+                        }
+                        Button {
+                            toggle_Privacy.toggle()
+                        } label: {
+                            Text("this amazing text")
+                        }
+                    }
+                }
+            }
+        }
     }
 }
 
