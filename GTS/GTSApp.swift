c1f63c25f8b8be4d98ca81135be5e971688001f9
Tristan <tristanchay123@gmail.com>
2023-11-22 09:51:05 +0800

    initial commit

---
+++ GTS/GTSApp.swift
@@ -0,0 +1,17 @@
+//
+//  GTSApp.swift
+//  GTS
+//
+//  Created by Tristan Chay on 22/11/23.
+//
+
+import SwiftUI
+
+@main
+struct GTSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
